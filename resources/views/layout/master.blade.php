a9e69f46002b5c9e73cbbd77a31ce0828b7e88aa
wdcoders <59465038+wdcoders@users.noreply.github.com>
2021-03-18 00:01:12 +0530

    first commit

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>E-Commerece</title>
+
+    {{-- bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+    {{-- scripts --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+</body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,4 @@
+@extends('./layout/master')
+@section('content')
+
+Login Page
